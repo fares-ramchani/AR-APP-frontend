7f9e4d54339b2851efb2128fe0019b959b7c7a3e
fares-ramchani <faresramchani@gmail.com>
2024-11-07 13:52:36 +0100

    test

--- src/screens/authentification/signup/Signup.tsx
+++ src/screens/authentification/signup/Signup.tsx
@@ -19,6 +19,7 @@ const Signup = ({navigation} : {navigation : any}) => {
           <Text style={styles.title}>SignUp</Text>
           <Text style={{color: 'gray'}}>
             Signup to your registered account.
+            Signup to your registered account
           </Text>
         </View>
         <View style={styles.body}>
