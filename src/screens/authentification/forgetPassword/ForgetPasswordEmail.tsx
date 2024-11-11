dbae49088bcad8b645a7f0e62312526fea0d7253
fares-ramchani <faresramchani@gmail.com>
2024-11-11 11:43:13 +0100

    screen forget password email v2 widh formik and yup /screen forget password code verification v2 widh formik and yup  /screen forget password reset password v2 widh formik and yup

--- src/screens/authentification/forgetPassword/ForgetPasswordEmail.tsx
+++ src/screens/authentification/forgetPassword/ForgetPasswordEmail.tsx
@@ -9,45 +9,89 @@ import {
   View,
 } from 'react-native';
 import {primary} from 'constants/Colors.ts';
+import * as yup from 'yup';
+import {Formik} from 'formik';
+
+const ForgetPasswordEmailForm = yup.object().shape({
+  email: yup
+    .string()
+    .email('Please enter valid email')
+    .required('Email Adress is required'),
+});
 
 const ForgetPasswordEmail = ({navigation}: {navigation: any}) => {
   return (
-    <View style={styles.container}>
-        <ScrollView contentContainerStyle={styles.scrollview}>
-      <View style={styles.header}>
-        <Image
-          source={require('asssets/img/Forgot-password-cuate.png')}
-          style={styles.imageStyle}></Image>
-      </View>
-      <View style={styles.body}>
-        <View style={styles.textcontainer}>
-          <Text style={styles.textStyle}>
-            Please Enter Your Email Address To Recieve a Verification Cord
-          </Text>
-        </View>
-        <View style={styles.conatinerInput}>
-          <Text style={styles.subtitle}>Email adress</Text>
-          <TextInput
-            placeholderTextColor="gray"
-            style={styles.input}
-            placeholder="Email"
-            keyboardType="email-address"
-          />
-          <View style={styles.textcontainer}>
-            <Text style={styles.textStyle2}>Try another way</Text>
-          </View>
-        </View>
+    <Formik
+      initialValues={{email: ''}}
+      validateOnMount={true}
+      validationSchema={ForgetPasswordEmailForm}
+      onSubmit={values => console.log(values)}>
+      {({
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        values,
+        touched,
+        errors,
+        isValid,
+      }) => (
+        <View style={styles.container}>
+          <ScrollView contentContainerStyle={styles.scrollview}>
+            <View style={styles.header}>
+              <Image
+                source={require('asssets/img/Forgot-password-cuate.png')}
+                style={styles.imageStyle}></Image>
+            </View>
+            <View style={styles.body}>
+              <View style={styles.textcontainer}>
+                <Text style={styles.textStyle}>
+                  Please Enter Your Email Address To Recieve a Verification Cord
+                </Text>
+              </View>
+              <View style={styles.conatinerInput}>
+                <Text style={styles.subtitle}>Email adress</Text>
+                <TextInput
+                  placeholderTextColor="gray"
+                  style={[
+                    styles.input,
+                    {
+                      backgroundColor:
+                        (errors.email && !touched.email) || isValid
+                          ? '#fff1ef'
+                          : 'red',
+                    },
+                  ]}
+                  placeholder="Email"
+                  keyboardType="email-address"
+                  onChangeText={handleChange('email')}
+                  onBlur={handleBlur('email')}
+                  value={values.email}
+                />
+                {errors.email && touched.email && (
+                  <Text style={styles.errorText}>{errors.email}</Text>
+                )}
+                <View style={styles.textcontainer}>
+                  <Text style={styles.textStyle2}>Try another way</Text>
+                </View>
+              </View>
 
-        <View style={styles.conatinerButtom}>
-          <TouchableOpacity style={styles.button} onPress={() =>
-              navigation.navigate('ForgetPasswordCodeVirification', {name: 'ForgetPasswordCodeVirification'})
-            }>
-            <Text style={{color: 'white'}}>Send</Text>
-          </TouchableOpacity>
+              <View style={styles.conatinerButtom}>
+                <TouchableOpacity
+                  style={[styles.button, {opacity: isValid ? 1 : 0.7}]}
+                  disabled={!isValid}
+                  onPress={() =>
+                    navigation.navigate('ForgetPasswordCodeVirification', {
+                      name: 'ForgetPasswordCodeVirification',
+                    })
+                  }>
+                  <Text style={{color: 'white'}}>Send</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </ScrollView>
         </View>
-      </View>
-      </ScrollView>
-    </View>
+      )}
+    </Formik>
   );
 };
 const styles = StyleSheet.create({
@@ -58,35 +102,33 @@ const styles = StyleSheet.create({
   scrollview: {
     flexGrow: 1,
     width: '100%',
+    justifyContent: 'center',
   },
 
   header: {
     alignItems: 'center',
     justifyContent: 'center',
-    height:280
-
+    height: 240,
   },
   body: {
-    height:400,
+    height: 400,
     padding: 25,
-    
   },
   conatinerInput: {
-    marginTop:30,
-    height:210,
+    marginTop: 30,
+    height: 210,
   },
   conatinerButtom: {
-    alignItems:'center',
-    justifyContent:'flex-end',
-    marginBottom:40,
-    width:'100%'
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    marginBottom: 40,
+    width: '100%',
   },
   imageStyle: {
     width: '70%',
     height: 220,
     borderRadius: 100,
-    backgroundColor: 'white',
-    shadowColor: primary,
+    shadowColor: 'white',
     shadowOffset: {
       width: 0,
       height: 1,
@@ -97,7 +139,7 @@ const styles = StyleSheet.create({
     elevation: 30,
   },
   textcontainer: {
-    height:70,
+    height: 70,
     width: '100%',
     justifyContent: 'center',
     alignItems: 'center',
@@ -106,12 +148,12 @@ const styles = StyleSheet.create({
     width: '80%',
     textAlign: 'center',
   },
-  textStyle2:{
+  textStyle2: {
     width: '80%',
     textAlign: 'center',
-    color:primary,
-     fontWeight: 'bold',
-    textDecorationLine: 'underline'
+    color: primary,
+    fontWeight: 'bold',
+    textDecorationLine: 'underline',
   },
   input: {
     width: '100%',
@@ -120,7 +162,6 @@ const styles = StyleSheet.create({
     margin: 0,
     borderRadius: 10,
     borderColor: primary,
-    backgroundColor: '#fff1ef',
   },
   subtitle: {
     fontSize: 16,
@@ -144,6 +185,12 @@ const styles = StyleSheet.create({
     shadowRadius: 2.22,
 
     elevation: 10,
+  },
+  errorText: {
+    color: 'red',
+    marginTop: 5,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
 export default ForgetPasswordEmail;
