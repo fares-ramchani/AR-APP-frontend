be69664eca3c83d0af2c22e648f3f968d1fef943
fares-ramchani <faresramchani@gmail.com>
2024-11-13 19:03:09 +0100

    homeScreen:Top Bar Navigation icon shop Screen name v1/Horizontal scroll view atest viewed products v1

--- src/screens/homescreen/HomeScreen.tsx
+++ src/screens/homescreen/HomeScreen.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import {StyleSheet, View,  Text} from 'react-native';
+import Carousel from 'components/Carousel.tsx';
+
+
+
 
 const HomeScreen = () => {
   return <View>
     <Text>
     HomeScreen
     </Text>
+    <Carousel />
   </View>;
 };
 
